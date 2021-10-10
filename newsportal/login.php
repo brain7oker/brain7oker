91497aae7d61bb8bcb28bb52ef997beae13609d4
henrylaufrans <henry.laufrans@student.umn.ac.id>
2021-10-11 01:23:19 +0700

    ya gt

--- newsportal/login.php
+++ newsportal/login.php
@@ -19,7 +19,7 @@ if(isset($_SESSION['logindulu'])){
     <title>News Portal | About us</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="css/modern-business.css" rel="stylesheet">
@@ -32,48 +32,12 @@ if(isset($_SESSION['logindulu'])){
  <!-- masukkin recaptcha didalem sini
  disinii
 -->
-<a href="login/index.php"><button class="btn first">As User</button></a>
+  <a href="login/index.php"><button class="btn first">As User</button></a>
 
-<a href="admin/index.php"><button class="btn first">As Admin</button></a>
+  <a href="admin/index.php"><button class="btn first">As Admin</button></a>
     <!-- Navigation -->
-    <?php include('includes/header.php');?>
-    <!-- Page Content -->
-    <div class="container">
-
-<?php 
-$pagetype='login';
-$query=mysqli_query($con,"select PageTitle,Description from tblpages where PageName='$pagetype'");
-while($row=mysqli_fetch_array($query))
-{
-
-?>
-      <h1 class="mt-4 mb-3"><?php echo htmlentities($row['PageTitle'])?>
-  
-      </h1>
-
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item">
-          <a href="index.php">Home</a>
-        </li>
-        <li class="breadcrumb-item active">About</li>
-      </ol>
-
-      <!-- Intro Content -->
-      <div class="row">
-
-        <div class="col-lg-12">
-
-          <p><?php echo $row['Description'];?></p>
-        </div>
-      </div>
-      <!-- /.row -->
-<?php } ?>
-    
-    </div>
-    <!-- /.container -->
-
-    <!-- Footer -->
- <?php include('includes/footer.php');?>
+  <?php include('includes/header.php');?>
+  <?php include('includes/footer.php');?>
 
   </body>
 
