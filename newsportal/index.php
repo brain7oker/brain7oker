fe7800cc77ac54945060ca3f38a9098c1b66939c
Frogice <ferdywilly99@yahoo.co.id>
2021-10-10 23:16:35 +0700

    Update index.php

--- newsportal/index.php
+++ newsportal/index.php
@@ -1,8 +1,7 @@
 <?php 
 session_start();
 include('includes/config.php');
-
-    ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -39,25 +38,19 @@ include('includes/config.php');
    ?>
 
     <!-- Page Content -->
-    <div class="container">
-
-
-     
+    <div class="container">  
       <div class="row" style="margin-top: 4%">
-
         <!-- Blog Entries Column -->
         <div class="col-md-8">
-
           <!-- Blog Post -->
 <?php 
-     if (isset($_GET['pageno'])) {
+        if (isset($_GET['pageno'])) {
             $pageno = $_GET['pageno'];
         } else {
             $pageno = 1;
         }
         $no_of_records_per_page = 8;
         $offset = ($pageno-1) * $no_of_records_per_page;
-
 
         $total_pages_sql = "SELECT COUNT(*) FROM tblposts";
         $result = mysqli_query($con,$total_pages_sql);
@@ -85,13 +78,7 @@ while ($row=mysqli_fetch_array($query)) {
             </div>
           </div>
 <?php } ?>
-       
-
-      
-
           <!-- Pagination -->
-
-
     <ul class="pagination justify-content-center mb-4">
         <li class="page-item"><a href="?pageno=1"  class="page-link">First</a></li>
         <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?> page-item">
@@ -102,27 +89,20 @@ while ($row=mysqli_fetch_array($query)) {
         </li>
         <li class="page-item"><a href="?pageno=<?php echo $total_pages; ?>" class="page-link">Last</a></li>
     </ul>
-
         </div>
-
         <!-- Sidebar Widgets Column -->
       <?php include('includes/sidebar.php');?>
       </div>
       <!-- /.row -->
-
     </div>
     <!-- /.container -->
-
     <!-- Footer -->
       <?php include('includes/footer.php');?>
-
-
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
  
-</head>
-  </body>
+  </head>
 
+  </body>
 </html>
