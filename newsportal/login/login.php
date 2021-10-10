d9510d1cc1a4fe40c70f22ac4e24e0025a9bbdc7
BrainJoker <charliefrederico7@gmail.com>
2021-10-10 23:05:52 +0700

    login

--- newsportal/login/login.php
+++ newsportal/login/login.php
@@ -66,46 +66,32 @@ echo "<script>alert('User not registered with us');</script>";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login Pesbuk</title>
+    <title>Login</title>
 
     <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
 </head>
-<body class="bg-light">
-
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-md-6">
-
-        <p>&larr; <a href="index.php">Home</a>
-
-        <h4>Masuk ke Pesbuk</h4>
-        <p>Belum punya akun? <a href="register.php">Daftar di sini</a></p>
-
-        <form action="" method="POST" >
-
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input class="form-control" type="text" name="username" placeholder="Username atau email" required/>
-            </div>
-
-
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input class="form-control" type="password" name="password" placeholder="Password" required/>
-            </div>
-
-            <input type="submit" class="btn btn-success btn-block" name="login" value="Masuk" required/>
-
+<body>
+    <div class="container">
+      <div class="wrapper">
+        <div class="title"><span>Login Form</span></div>
+        <form action="#">
+          <div class="row">
+            <i class="fas fa-user"></i>
+            <input type="text" placeholder="Email or Phone" required>
+          </div>
+          <div class="row">
+            <i class="fas fa-lock"></i>
+            <input type="password" placeholder="Password" required>
+          </div>
+          <div class="row button">
+            <input type="submit" value="Login">
+          </div>
+          <div class="signup-link">Not a member? <a href="register.html">Signup now</a></div>
         </form>
-            
-        </div>
-
-        <div class="col-md-6">
-            <!-- isi dengan sesuatu di sini -->
-        </div>
-
+      </div>
     </div>
-</div>
-    
-</body>
+
+  </body>
 </html>
