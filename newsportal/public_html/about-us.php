d1d7b8d023e8c63f349000fc1a07a1d84b6d29a7
BrainJoker <charliefrederico7@gmail.com>
2021-10-11 16:45:17 +0700

    about-us

--- newsportal/public_html/about-us.php
+++ newsportal/public_html/about-us.php
@@ -20,6 +20,8 @@ session_start();
 
     <!-- Custom styles for this template -->
     <link href="css/modern-business.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="about-us.css" />
   </head>
 
   <body>
@@ -54,26 +56,63 @@ while($row=mysqli_fetch_array($query))
         </li>
         <li class="breadcrumb-item active">About</li>
       </ol>
+      <section class="pa-4">
 
-      <!-- Intro Content -->
-      <div class="row">
+		<div id="title">
+			<div id="subtitle">Our Works</div>
+			<div id="text">
+				Hai semua, Terima Kasih sudah mengujungi web kita. Kami selaku CEO dan CTO serta karyawan dari Catering Maju Jaya mengucapkan semoga kalian puas dengan website ini dan dapat menggunakannya dengan baik.<br>
+				Sedikit Cerita mengenai Catering Maju Jaya. Catering Maju Jaya diciptakan oleh 2 orang yang sekarang menjabat sebagai CEO dan CTO di Catering Maju Jaya. Mereka mengembangkan usaha ini dengan tujuan mendapatkan nilai dari mata kuliah PTI di UNIVERSITAS MULTIMEDIA NUSANTARA.<br>
+				Usaha Catering Maju Jaya ini kami harap menjadi usaha yang sangat sukses di Tangerang dan akhirnya melebarkan sayap ke seluruh nusantara dan menjadi catering nomor 1 di Indonesia.
+			</div><br>
+		</div>
 
-        <div class="col-lg-12">
 
-          <p><?php echo $row['Description'];?></p>
-        </div>
-      </div>
-      <!-- /.row -->
+		<div id="mt-5 mb-5">
+			<div id="subsubtitle">Meet Our Team</div><br>
+			<div id="gridlist">
+
+
+			  <div id="item">
+			  	
+			  	<img src="gambar/charlie.jpg" alt="Image" height="200" width="200">
+			  	<p class="subjudul">Charlie Frederico</p>
+
+			  </div>
+
+			  <div id="item">
+			  	
+			  	<img src="gambar/henry.jpg" alt="Image" height="200" width="200">
+
+			  	<p class="subjudul">Henry Laufrans</p>
+
+			  </div>
+
+			  <div id="item">
+			  	
+				<img src="gambar/christian.png" alt="Image" height="200" width="200">
+				<p class="subjudul">Christian Alexander</p>
+
+			</div>
+
+        <div id="item">
+			  	
+			  	<img src="gambar/ferdy.jpg" alt="Image" height="200" width="200">
+			  	<p class="subjudul">Ferdy Willy</p>
+
+			  </div>
+
+			</div>
+		</div>
+	</section>
 <?php } ?>
-    
-    </div>
-    <!-- /.container -->
-    <!-- Footer -->
- <?php include('includes/footer.php');?>
 
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   
   </body>
+    <footer class="py-5 bg-dark">Copyright © CCFH 2021
+      <div class="container"></div>
+    </footer>
 </html>
